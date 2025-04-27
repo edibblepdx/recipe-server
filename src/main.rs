108a706df08a3fd0396c34697b77c30fb433ce5e
ethan dibble <edibble@pdx.edu>
2025-04-26 21:13:29 -0700

    tracing subscriber

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,53 @@
 mod recipe;
 mod templates;
 
-use axum::{Router, routing::get};
+use recipe::*;
+use templates::*;
+
+use axum::{Router, routing};
 use clap::Parser;
-use tokio;
+use sqlx::{SqlitePool, migrate::MigrateDatabase, sqlite};
+use tokio::{net, sync::RwLock};
+use tower_http::{services, trace};
+use tracing_subscriber::{layer::SubscriberExt, util::SubscriberInitExt};
+
+use std::sync::Arc;
 
 #[derive(Parser)]
 struct Args {}
 
+struct AppState {
+    //db: SqlitePool,
+}
+
+async fn root() {}
+
 async fn serve(args: Args) -> Result<(), Box<dyn std::error::Error>> {
+    let current_recipe = Recipe::default();
+
+    //let app_state = AppState { db };
+    let app_state = AppState {};
+    let state = Arc::new(RwLock::new(app_state));
+
+    tracing_subscriber::registry()
+        .with(
+            tracing_subscriber::EnvFilter::try_from_default_env()
+                .unwrap_or_else(|_| "recipe-server=debug,info".into()),
+        )
+        .with(tracing_subscriber::fmt::layer())
+        .init();
+    // https://carlosmv.hashnode.dev/adding-logging-and-tracing-to-an-axum-app-rust
+    let trace_layer = trace::TraceLayer::new_for_http()
+        .make_span_with(trace::DefaultMakeSpan::new().level(tracing::Level::INFO))
+        .on_response(trace::DefaultOnResponse::new().level(tracing::Level::INFO));
+
+    let app = Router::new()
+        .route("/", routing::get(root))
+        .layer(trace_layer)
+        .with_state(state);
+
+    let listener = net::TcpListener::bind("127.0.0.1:3000").await?;
+    axum::serve(listener, app).await?;
     Ok(())
 }
 
--- src/recipe.rs
+++ src/recipe.rs
@@ -1,1 +1,13 @@
-pub struct Recipe {}
+pub struct Recipe {
+    id: u64,
+    name: String,
+}
+
+impl Default for Recipe {
+    fn default() -> Self {
+        Self {
+            id: 0,
+            name: "default".to_string(),
+        }
+    }
+}
